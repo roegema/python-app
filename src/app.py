48b9b3736167a9cb3883105558696b8417a1f090
Rene Oegema <rene.oegema@ing.com>
2026-01-30 12:30:57 +0100

    wip2

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def info():
     return jsonify({
         'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
         'hostname': socket.gethostname(),
-        'message': 'Updated on Fri, Jan 30th',
+        'message': 'My update on: 30-01-2026 12:30',
         'deployed_on': 'kubernetes'
     })
 
