e34b51e5355822a8a5ced538eef3c79ee158e3a6
Rene Oegema <oegjes@gmail.com>
2026-05-01 17:35:45 +0200

    wip 25, GitHub Actions Backstage Techdocs

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def info():
     return jsonify({
         'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
         'hostname': socket.gethostname(),
-        'message': 'My update on: 01-05-2026 wip 22, Backstage Techdocs',
+        'message': 'My update on: 01-05-2026 wip 25, GitHub Actions Backstage Techdocs',
         'deployed_on': 'kubernetes'
     })
 
